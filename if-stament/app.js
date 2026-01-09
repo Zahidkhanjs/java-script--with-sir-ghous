64a340b07af985a6bc44b7e2aa7ede07873cd98e
zahidkhanjs <zahidshahwani9925@gmail.com>
2026-01-09 22:07:40 +0500

    edit

---
+++ if-stament/app.js
@@ -0,0 +1,105 @@
+
+// var gender = prompt( "Enter your gender" , "Male")
+
+// if(gender === "Male "){
+//     alert( "hello , sir")
+
+// }
+// if( gender === "female "){
+//     alert( "hello , sister")
+// }
+
+
+
+// let gender =  prompt ( "Enter your gender " , "Male")
+ 
+// if( gender === "Male"){
+//     alert( "hello , sir")
+// }
+
+// if( gender === "Female"){
+//     alert ( "hello , sister")
+// }
+
+
+
+// var gender =  prompt ( " Enter your gender " , "Female")
+
+// if (gender === "Female"){
+//     alert ("hello , sister")
+// }
+
+// if (gender === "Male"){
+//     alert ( "hello ,sir")
+// }
+  
+
+
+
+
+// var gender = prompt("Enter your name" , "Male")
+// var message = "Note Match";
+// if (gender === "Male"){
+//    message = ("hello , sir")
+// }
+
+// if (gender === "Female"){
+//     message = ("hello , Sister")
+// }
+// alert( message )\
+
+
+
+
+// var education = prompt ( " Enter your education ")
+
+// if (education !== "Matric"){
+//     alert("Eligbale")
+
+// }
+
+
+//  var english = prompt("Enter your englsh leavel ")
+
+//  if (english !== "leavel 2"){
+//     console.log("good job");
+
+//  }
+
+
+// var age = prompt (" age is grater the twenty ")
+ 
+// if(age !== "<20"){
+//     alert("you are eligble ")
+
+// }
+
+
+// var age = 21
+
+// if(age > 21){
+//     console.log("are your fine");
+// }
+
+
+
+
+
+// var kiahy = prompt( "kuch lay ha ho")
+
+// if(kiahy === "dalhy"){
+//     console.log("lobia lay hana");
+// }
+// else if (kiahy === "chawel"){
+//     console.log("pir bans pati lay hana");
+// }
+// else if (kiahy === "frout"){
+//     console.log("pir ornage lay hana ");
+
+// }
+// else{
+//     console.log("bus kuch bie lay hana");
+// }
+
+
+var gar = hello
